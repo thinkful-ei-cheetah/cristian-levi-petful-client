7cd04e673f099ba3ce6e41004811531b7ce7bf34
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-06-21 09:31:57 -0700

    set up routes, components

---
+++ src/components/App/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Header from '../Header/Header'
+import LandingPage from '../../routes/LandingPage/LandingPage'
+import AdoptionPage from '../../routes/AdoptionPage/AdoptionPage'
+import './App.css';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className='App'>
+        <header role='banner' className='App-header'>
+          <Header />
+        </header>
+        <main role='main' className='App-main'>
+          <Switch>
+            <Route 
+              exact
+              path={'/'}
+              component={LandingPage}
+            />
+
+            <Route 
+              path={'/adopt'}
+              component={AdoptionPage}
+            />
+
+          </Switch>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
